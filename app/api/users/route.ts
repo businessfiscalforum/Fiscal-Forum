fb1a7e94740153344f41b0a563ead71f06fc6784
saumya <saumya20personal@gmail.com>
2025-08-23 14:46:38 +0530

    change75b

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -17,6 +17,7 @@ function corsHeaders(origin: string | null): HeadersInit {
       "Access-Control-Allow-Origin": origin,
       "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
       "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      "Access-Control-Allow-Credentials": "true"
     };
   }
   return {};
