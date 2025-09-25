78a5247cca10f14dcb1c3b34679f3cd4866f39e7
Shashank Gupta <shashankg2705@gmail.com>
2025-09-25 17:29:22 +0530

    change137d

--- app/(routes)/news/[id]/page.tsx
+++ app/(routes)/news/[id]/page.tsx
@@ -169,8 +169,7 @@ export default async function NewsDetailPage(context: {
             <div className="relative w-full h-64 sm:h-80 md:h-96">
               <Image
                 src={news.image}
-                width={400}
-                height={450}
+                fill
                 alt={`Featured image for ${news.title}`}
                 className="object-cover"
                 sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
