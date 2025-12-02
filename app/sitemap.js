7243a242539e8229fbd7ef4e14e23ef482dcc1bf
saumya <saumya20personal@gmail.com>
2025-12-02 23:31:08 +0530

    sitemap edit2

--- app/sitemap.js
+++ app/sitemap.js
@@ -12,7 +12,7 @@ export default function sitemap() {
     { url: `${base}/about`, lastModified: new Date() },
     { url: `${base}/referrals`, lastModified: new Date() },
     { url: `${base}/refunds`, lastModified: new Date() },
-    { url: `${base}/terms-&-conditions`, lastModified: new Date() },
+    { url: `${base}/terms-&amp;-conditions`, lastModified: new Date() },
     { url: `${base}/services/stock-investment`, lastModified: new Date() },
     { url: `${base}/services/mutual-funds`, lastModified: new Date() },
     { url: `${base}/services/insurance`, lastModified: new Date() },
